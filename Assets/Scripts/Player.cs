31188e4ab8413283b0ed0db140232509c6d8ce39
smallquasar <data2inv@gmail.com>
2020-04-28 17:27:31 +0300

    Init commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 2.5f;
+
+    private Vector3 _direction;
+
+    private void Start()
+    {
+        //_direction = new Vector3(0, _rb.velocity.y, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        _direction.x = Input.GetAxis("Horizontal") * _speed;
+        _direction.z = Input.GetAxis("Vertical") * _speed;
+        _direction.y = _rb.velocity.y;
+
+        _rb.velocity = _direction;
+
+        Debug.Log("Velocity Y: " + _rb.velocity.y);
+    }
+}
---
+++ Assets/Scripts/RouteFollower.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouteFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _traveler;
+    [SerializeField] private float _speed = 4f;
+
+    private Node[] _routeNodes;
+    private int _nodesCount = 0;
+
+    private Vector3 _targetRoutePosition = Vector3.zero;
+    private int _currentNodeIndex = -1;
+    private bool _isDirectRoute = true;
+
+    private void Start()
+    {
+        _routeNodes = GetComponentsInChildren<Node>();
+        _nodesCount = _routeNodes != null ? _routeNodes.Length : 0;
+
+        if (_nodesCount > 0)
+            DefineTargetNode();
+    }
+
+    private void DefineTargetNode()
+    {
+        if (_isDirectRoute)
+            DefineTargetNodeDirect();
+        else
+            DefineTargetNodeReturn();
+
+        _targetRoutePosition = _routeNodes[_currentNodeIndex].transform.position;
+    }
+
+    private void DefineTargetNodeDirect()
+    {
+        if (_currentNodeIndex < _nodesCount - 1)
+        {
+            _currentNodeIndex++;
+        }
+        else
+        {
+            _currentNodeIndex--;
+            _isDirectRoute = false;
+        }
+    }
+
+    private void DefineTargetNodeReturn()
+    {
+        if (_currentNodeIndex > 0)
+        {
+            _currentNodeIndex--;
+        }
+        else
+        {
+            _currentNodeIndex++;
+            _isDirectRoute = true;
+        }
+    }
+
+    void Update()
+    {
+        if (_nodesCount == 0)
+            return;
+
+        _traveler.position = Vector3.MoveTowards(_traveler.position, _targetRoutePosition, _speed * Time.deltaTime);
+
+        if (_traveler.position == _targetRoutePosition)
+        {
+            DefineTargetNode();
+        }
+    }
+}
