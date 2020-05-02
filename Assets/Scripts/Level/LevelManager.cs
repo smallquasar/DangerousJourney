1813801ea77e614b06b7955305f4cadb2eb7b459
smallquasar <data2inv@gmail.com>
2020-05-02 13:44:09 +0300

    Added level manager and level result check

---
+++ Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager LvlManager { get; private set; }
+
+    public string EnemyTag { get; private set; }
+
+    private void Awake()
+    {
+        if (!LvlManager)
+        {
+            DontDestroyOnLoad(gameObject);
+            LvlManager = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        EnemyTag = "Enemy";
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (SceneManager.sceneCount > nextSceneIndex)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/Level/LevelResultCheck.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LevelResultCheck : MonoBehaviour
+{
+    private string _enemyTag;
+
+    private void Start()
+    {
+        _enemyTag = LevelManager.LvlManager.EnemyTag;
+    }
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag(_enemyTag))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
