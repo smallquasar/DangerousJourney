5e3e483712aa79ef870351561b0ebcbe569d6d4c
smallquasar <data2inv@gmail.com>
2020-05-05 17:57:06 +0300

    Added UI navigation by the controller

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private Button _restartLevelButton;
+    [SerializeField] private Button _nextLevelButton;
+    [SerializeField] private Button _exitGameButton;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("RestartLevel"))
+        {
+            _restartLevelButton.onClick.Invoke();
+        }
+        else if (Input.GetButtonDown("NextLevel"))
+        {
+            _nextLevelButton.onClick.Invoke();
+        }
+        else if (Input.GetButtonDown("ExitGame"))
+        {
+            _exitGameButton.onClick.Invoke();
+        }
+    }
+}
